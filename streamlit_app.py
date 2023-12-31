16bdf2461777a7c5eb00bea4437ffa210c57d4cb
ProHaller <88140523+ProHaller@users.noreply.github.com>
2023-12-31 14:03:27 +0900

    Titles

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,34 +11,9 @@ from st_audiorec import st_audiorec
 
 def display_readme():
     st.image("static/transcription.svg", width=600)
-    st.markdown(
-        """
-#  Welcome to Roland Tools
-
-This application utilizes OpenAI's powerful models for audio transcription and text processing. To get started, please follow these simple steps:
-
-### ↖️ Mobile Users may need to open the side panel  
-
-## Using the App
-### Transcription
-- **Upload or record an Audio File**: Choose an audio file (mp3, wav, or m4a format).
-- **Choose the Language**: Select the language of the audio file.
-- **Optional Description**: Provide a brief description of the audio and the unusual vocabulary for better context and transcription.
-- **Transcription Format**: Choose between plain text or subtitles (SRT format).
-- **Transcribe**: Click the 'Transcribe' button to start the transcription process.
-
-### Text Post-processing
-- **Input Your Prompt**: Type or paste the prompt with the instructions for the Post-processing in the prompt text area.
-- **Choose a Model**: Select either GPT-4 or GPT-3.5 based on your needs.
-- **Set the Temperature**: Adjust the slider to set the model's creativity.
-- **Process Text**: Click the 'Process text' button to start processing the transcription text.
-
-## Download Results
-- After transcription or text processing, you can download the results using the 'Download' button.
-
-Enjoy your experience with Roland Tools! If you have any questions or feedback, please feel free to reach out.
-    """
-    )
+    with open("README.md", "r") as file:
+        readme_content = file.read()
+    st.markdown(readme_content)
 
 
 # Check if the user has seen the README
@@ -203,9 +178,8 @@ with st.sidebar:
 
     tab1, tab2 = st.tabs(["Transcription", "Text processing"])
     with tab1:
-        st.header("Choose an audio file")
+        st.header("💽 Upload an Audio")
         uploaded_file = st.file_uploader(
-            "Upload an audio file ⬇️",
             type=[
                 "flac",
                 "m4a",
@@ -219,7 +193,7 @@ with st.sidebar:
                 "webm",
             ],
         )
-        st.header("You can also directly record some audio")
+        st.header("🎙️Record an Audio")
         recorded_file = st_audiorec()
         transcribe_button = None
         if uploaded_file or recorded_file:
