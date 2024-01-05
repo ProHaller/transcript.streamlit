0f69c48768c5be84249d4ad7b64404f5f579881d
ProHaller <88140523+ProHaller@users.noreply.github.com>
2024-01-06 00:30:02 +0900

    resizing pictures

--- streamlit_app.py
+++ streamlit_app.py
@@ -124,7 +124,7 @@ def display_readme(lang_code="en"):
             " from it.\n"
             "    "
         )
-        st.image("static/transcription.svg", width=300)
+        st.image("static/transcription.svg", width=500)
         st.markdown(welcome_text)
         file_name = "README_JP.md" if lang_code == "ja" else "README.md"
         with open(file_name, "r") as file:
